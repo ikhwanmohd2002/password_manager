1d25f18edeed640164ac8aa9a2241e351a2e9e3a
ikhwanmohd2002 <ikhwanmohd2002@gmail.com>
2024-10-22 20:43:48 +0800

    Change IP

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,5 +1,5 @@
 class API {
-  static const hostConnect = "http://192.168.1.118/api_password_manager";
+  static const hostConnect = "http://192.168.1.152/api_password_manager";
   static const hostConnectUser = "$hostConnect/user";
   static const hostConnectPassword = "$hostConnect/password";
   static const hostConnectSharedPassword = "$hostConnect/shared_password";
